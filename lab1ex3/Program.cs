aaf4e7dfc85139dced9b7abbb1541d0c4c224a5d
uimkatali <b8bf6b5@gmail.com>
2023-09-17 21:06:18 +0300

    Add project files.

---
+++ lab1ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace lab1ex1
+{
+/*    Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+tastaura
+*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(number % 10);
+        }
+    }
+}
